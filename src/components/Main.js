f060c341a6e02424b9e3133cc33508b1a6b9863e
Matthew Bryce <mattbryce93@gmail.com>
2020-04-27 17:12:19 +1200

    Added: mailto link for contact form

--- src/components/Main.js
+++ src/components/Main.js
@@ -111,29 +111,14 @@ class Main extends React.Component {
           id="contact"
           className={`${this.props.article === 'contact' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
-          }`}
+            }`}
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
+          <form method="GET" action="mailto:hayleysaunders09@gmail.com">
             <ul className="actions">
               <li>
                 <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
               </li>
             </ul>
           </form>
